f3cdfa027603faee70393a93ee938540af8443c6
Jelgnum <86729439+Jelgnum@users.noreply.github.com>
2021-12-08 14:14:31 -0600

    Fixed .jar name and added comments
    Gave more explanation to java args and more comments for explanation of java args

--- run.sh
+++ run.sh
@@ -3,11 +3,15 @@
 screen -d -m -S "Minecraft Server"
 
 #Pushes server launch commands to the screen instance
+#Edit all the stuff with <>, remove the <> after
+#<X> is the RAM in gigabytes you want to allocate, it's recommended to have both the same for garbage collection reasons
+#But you can set them differently if needed -Xms is the minimum, -Xmx is the maximum
 
-screen -x "Minecraft Server" -p 0 -X stuff " java -XmsXG -XmxXG -jar forge-<Version>-universal.jar nogui\n"
+screen -x "Minecraft Server" -p 0 -X stuff " java -Xms<X>G -Xmx<X>G -jar <Server Name>.jar nogui\n"
 
 #Launches terminal window
 
 Konsole -e screen -r
 #Gnome-terminal -e screen -r
+#<terminal launch command> -e screen -r
 #Put whatever terminal you use otherwise
